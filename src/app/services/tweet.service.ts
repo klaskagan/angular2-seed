04e65740514e1cabf2326a051754eaa0ba30fee1
klaskagan <klaskagan@gmail.com>
2016-11-06 11:30:08 +0200

    ngClass can be used instead of [class.my-css-class]. Instead of using class binding twice, we can simply use ngClass directive. Using ngClass make your code cleaner and more maintainable

--- src/app/services/tweet.service.ts
+++ src/app/services/tweet.service.ts
@@ -8,7 +8,7 @@ export class TweetService {
 
   constructor() {
     var tweet1 = new Tweet("ARSTOT","Come on you gunners!","klaskagan", 30);
-    var tweet2 = new Tweet("TRUMP is a giant duche", "You all f*!kers will shit your pants!", "skankhunt42", -9);
+    var tweet2 = new Tweet("TRUMP is a giant duche", "You all f*!kers will shit your pants!", "skankhunt42", 0);
     var tweet3 = new Tweet("AngularJS News", "This framework looks awesome", "rp33", 4);
     this.tweets = [tweet1, tweet2, tweet3];
   }
