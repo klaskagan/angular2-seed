c4c3da4a3a8c5e67d127ecffdadb9c5168161e74
klaskagan <klaskagan@gmail.com>
2016-11-07 20:46:19 +0200

    Improved packaging infrastructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {CoursesComponent} from "./courses/courses.component";
-import { AuthorsComponent } from './authors/authors.component';
+import {CoursesComponent} from "./components/courses/courses.component";
+import { AuthorsComponent } from './components/authors/authors.component';
 import {AutoGrowDirective} from "./directives/auto-grow.directive";
 import { StarComponent } from './components/star/star.component';
 import { LikeComponent } from './components/like/like.component';
